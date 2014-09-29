163e32f260e944ae565781850dcdc4b57458d8ad
Alejandro El Informatico <aeinformatico@gmail.com>
2014-09-29 20:16:11 +0200

    CHANGE use SET for all values
    This improves code, error and data handling

--- lib/featurer/adapters/redis.rb
+++ lib/featurer/adapters/redis.rb
@@ -14,39 +14,24 @@ module Featurer
       delete_key(feature)
     end
 
-    def key(name)
-      "#{@config[:prefix]}:feature:#{name}"
-    end
-
-    def on?(feature, user_id = nil)
-      if user_id.nil?
-        fetch_key(feature)
-      else
-        fetch_from_set(feature, user_id)
-      end
+    def on?(feature, value = true)
+      fetch_from_set(feature, value)
     end
 
     def register(name, value = true)
-      # ensure old key is deleted in order to
-      # prevent performing an action in a different
-      # redis type: Redis::CommandError
+      # ensure old data is wiped
       delete(name)
-
-      if !value.kind_of?(Array)
-        save_key(name, value)
-      else
-        save_set(name, value)
-      end
+      save_set(name, value)
     end
 
     private
 
-    def delete_key(name)
-      @redis.del key(name)
+    def key(name)
+      "#{@config[:prefix]}:feature:#{name}"
     end
 
-    def save_key(name, value)
-      @redis.set key(name), value
+    def delete_key(name)
+      @redis.del key(name)
     end
 
     def save_set(name, value)
@@ -55,10 +40,6 @@ module Featurer
 
     def fetch_from_set(name, id)
       @redis.sismember(key(name), id)
-    end
-
-    def fetch_key(name)
-      @redis.get(key(name)) == 'true'
     end
   end
 end
